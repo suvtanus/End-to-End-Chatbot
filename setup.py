c36f50b9245874de8152d227b1522c8d48129627
Tanu Behera <suvtanus@gmail.com>
2025-02-25 13:35:40 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project tsb',
+    version= '0.0.0',
+    author= 'Tanu Behera',
+    author_email= 'tanu.behera@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
