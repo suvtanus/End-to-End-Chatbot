788102f9cd57b5bfda7d31d8beba269c1044501c
Tanu Behera <suvtanus@gmail.com>
2025-03-21 18:02:32 +0530

    updated helper.py

--- src/helper.py
+++ src/helper.py
@@ -1,4 +1,4 @@
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 # from langchain.embeddings import HuggingFaceEmbeddings # Depreciated libraries
 from langchain_community.embeddings import HuggingFaceEmbeddings
